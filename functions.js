78a0bb35a5aa60f4e71eb9858ed69ef9683521fa
mireiaGomariz <mireiapgomariz@gmail.com>
2020-01-23 12:13:00 +0100

    objects and dom

---
+++ functions.js
@@ -0,0 +1,66 @@
+//HELLO WORLD
+
+// let name = "Mireia";
+//
+//
+// function sayHelloWorld () {
+//   console.log("Hello World" + name);
+// };
+//
+// sayHelloWorld();
+//
+
+
+
+// function sayHelloWorld (name) {
+//
+//   console.log("Hello World" + name);
+// };
+//
+// sayHelloWorld("Mireia");
+
+
+// function sayHello(name1) {
+//   return "Hello" + name1;
+//   //reverse the letters of name1
+//   //convert name1 into capital letters
+//   //add an emoji at the end
+//
+// };
+//
+// sayHello("Mireia");
+//
+// console.log(sayHello("Mireia"));
+
+//
+// function twiceTheNumber(num) {
+//   let result = num * 2;
+//   return result;
+//
+// };
+// //
+// // console.log(twiceTheNumber(2))
+//
+// let finalResult = twiceTheNumber(2);
+//
+// console.log("The final result is " + finalResult);
+
+//function inside a variable
+//
+// let multiplication = function twiceTheNumber(num) {
+//   let result = num * 2;
+//   return result;
+//
+// };
+//
+// console.log("The final result is " + multiplication(4));
+
+
+
+//return the value of a multiplication function of two numbers.
+//
+// function multiplication(num1, num2) {
+//   return num1 * num2;
+// }
+//
+// console.log(multiplication(3,5));
